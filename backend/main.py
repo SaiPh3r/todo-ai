3fbb83f8625f38a4f823d2d630db1deba826662d
SaiPh3r <saianand750@gmail.com>
2025-10-26 20:22:05 +0530

    cors , user creating get logic

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
 from pydantic import BaseModel , Field
 from typing import List
+import json
+from fastapi.middleware.cors import CORSMiddleware
 
+app = FastAPI()
+# CORS setup
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],  # <--- important
+    allow_headers=["*"],  # <--- important
+)
 
 class Todo(BaseModel):
     id:str
@@ -11,10 +27,33 @@ class Todo(BaseModel):
 class User(BaseModel):
     user_id:str
     email:str
-    todos: List[Todo] # here i defined that list is of type todo
+    todos: List[Todo] = [] # here i defined that list is of type todo
 
-app = FastAPI()
+def load_data():
+    with open("data.json" , "r") as f:
+        data = json.load(f)
+    return data
+
+
+def save_data(data):
+    with open("data.json", "w") as f:
+        json.dump(data, f)
+
+
 
 @app.get("/")
 def home():
     return {"message":"hello world"}
+
+@app.get("/user")
+def get_or_create_user(user_id: str, email: str):
+    data = load_data()
+    print(data)
+    for user in data["users"]:
+        if(user["user_id"] == user_id):
+            return user
+        
+    new_user = User(user_id=user_id , email=email)
+    data["users"].append(new_user.dict())
+    save_data(data)
+    return new_user
