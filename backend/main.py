01036840d2bbd730013dabfc30d8907da7f755da
SaiPh3r <saianand750@gmail.com>
2025-10-26 19:23:15 +0530

    todo and user pydantic class made and data.json added

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,17 @@
 from fastapi import FastAPI
+from pydantic import BaseModel , Field
+from typing import List
+
+
+class Todo(BaseModel):
+    id:str
+    text:str
+    completed:bool = Field(default=False)
+
+class User(BaseModel):
+    user_id:str
+    email:str
+    todos: List[Todo] # here i defined that list is of type todo
 
 app = FastAPI()
 
